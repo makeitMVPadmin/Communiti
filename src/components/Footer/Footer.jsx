b601b253bd1e6cd6d8f73b7486179484bbc7582e
Aurelie Gedeon <agedeon540@gmail.com>
2023-10-30 03:23:36 -0500

    87-AG Added communiti logo to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,14 @@ import "./Footer.scss"
 import instagram from "../../assets/logos/instagram.svg"
 import twitter from "../../assets/logos/twitter.svg"
 import linkedin from "../../assets/logos/linkedin.svg"
+import communiti2 from "../../assets/logos/communiti2.svg"
 
 export default function Footer() {
     return(
         <>
             <div className="footer">
+                <img className="logo" src={communiti2} alt="communiti in black font" />
+                <div className="separator"></div>
                 <div className="icon-row">
                     <a href="#">
                         <img src={instagram} alt="instagram" />
