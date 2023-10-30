6bd24f46be8257f304cb15dbb1694a934e0a864a
Aurelie Gedeon <agedeon540@gmail.com>
2023-10-30 02:35:47 -0500

    87-AG added logo svgs and tried to create separator

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
 import "./Footer.scss"
+import instagram from "../../assets/logos/instagram.svg"
+import twitter from "../../assets/logos/twitter.svg"
+import linkedin from "../../assets/logos/linkedin.svg"
 
 export default function Footer() {
     return(
         <>
-        <p>Hello</p>
+            <div className="footer">
+                <div className="icon-row">
+                    <a href="#">
+                        <img src={instagram} alt="instagram" />
+                    </a>
+                    <a href="#">
+                        <img src={twitter} alt="twitter" />
+                    </a>
+                    <a href="#">
+                        <img src={linkedin} alt="linkedin" />
+                    </a>
+                </div>
+            </div>
         </>
     )
 }
