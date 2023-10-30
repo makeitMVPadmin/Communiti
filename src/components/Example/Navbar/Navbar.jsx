c945cee58172d2bef96355c9590d23b51179dd9f
Aurelie Gedeon <agedeon540@gmail.com>
2023-10-29 22:57:54 -0500

    started navbar

---
+++ src/components/Example/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss"
+
+export default function Navbar () {
+    return(
+        <>
+            <nav className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Log In </li>
+                    <li>Signup</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import "./HomePage.scss";
+import Navbar from "../../components/Example/Navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="home">
       <h1 className="home__title"> Welcome !</h1>
+      <Navbar />
     </div>
   );
 };
