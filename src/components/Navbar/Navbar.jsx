16bb6975c085cde63de2f2d7eb098d6d392b8c80
Aurelie Gedeon <agedeon540@gmail.com>
2023-10-30 02:51:10 -0500

    93-AG Added logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import "./Navbar.scss"
 import Button from "../Button/Button"
+import communiti from "../../assets/logos/communiti.svg"
 
 export default function Navbar () {
 
     return(
         <>
             <nav className="navbar">
+                <a href="#">
+                    <img src={communiti} alt="communiti" />
+                </a>
                 <ul className="nav-links">
                     <li>Home</li>
                     <li>About</li>
