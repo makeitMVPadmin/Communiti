2ba19716c573125daf57d69ae1da4ac374e04c18
Ana Bennett <anabennett1111@gmail.com>
2023-10-31 09:50:42 -0500

    Creating a branch for the styling and mixins

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,21 @@
-import "./Home.scss";
+// import "./HomePage.scss";
 
-const Home = () => {
+const HomePage = () => {
   return (
     <div className="home">
-      <h1 className="home__title"> Welcome !</h1>
+      <h1> Welcome !</h1>
+      <div className="home_container"> 
+        <button>button number 1</button>
+        <h1>lalala</h1>
+      </div>
+      <div>
+        <p>lalalalalalalalalalala</p>
+        <button>button number 2</button>
+      </div>
+      <button> button number 3</button>
     </div>
+
   );
 };
 
-export default Home;
+export default HomePage;
