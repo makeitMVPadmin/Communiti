dd4d70c4d5125e414f8c9a71ad2503a9210b905e
Ana Bennett <anabennett1111@gmail.com>
2023-11-02 13:01:43 -0500

    Change classnames in the how we do it section

--- src/components/HowWeDoIt.jsx
+++ src/components/HowWeDoIt.jsx
@@ -8,13 +8,13 @@ import "./HowWeDoIt.scss";
 
 const HowWeDoIt = () => {
   return (
-    <div className="home">
-      <div className="home_content">
-        <div className="home_icon_container">
+    <div className="how">
+      <div className="how_content">
+        <div className="how_icon_container">
           <img src={hands} alt="hands" />
         </div>
-        <h1 className="home_header">HOW WE DO IT</h1>
-        <div className="home_images">
+        <h1 className="how_header">HOW WE DO IT</h1>
+        <div className="how_images">
           <img src={events} alt="events" />
           <img src={inbox} alt="inbox" />
           <img src={update} alt="update" />
