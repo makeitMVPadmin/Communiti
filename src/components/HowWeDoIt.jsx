e2f31fc39ce7a45751a6ea7e9bf870dbc05228cf
Ana Bennett <anabennett1111@gmail.com>
2023-11-03 12:53:31 -0500

    Implement the tablet size mixins

--- src/components/HowWeDoIt.jsx
+++ src/components/HowWeDoIt.jsx
@@ -10,39 +10,41 @@ const HowWeDoIt = () => {
     <div className="how">
       <div className="how_content">
         <div className="how_icon_container">
-          <img src={hands} alt="hands" />
+          <img className="how_hands" src={hands} alt="hands" />
         </div>
         <h2 className="how_header">HOW WE DO IT</h2>
         <div className="how_images">
-          <img className="how_image" src={calendar} alt="events" />
-          <div className="left-align-section">
-            {" "}
-            {/* Apply the class to the section */}
-            <h3 className="underlined">Events</h3>
-            <p className="paragraph">
-              Stop forgetting your upcoming events. Track them all with your
-              Communiti calendar.
-            </p>
+          <div className="how_event_section">
+            <img className="how_image" src={calendar} alt="events" />
+            <div className="left-align-section">
+              <h3 className="underlined">Events</h3>
+              <p className="paragraph">
+                Stop forgetting your upcoming events. Track them all with your
+                Communiti calendar.
+              </p>
+            </div>
           </div>
-          <img className="how_image" src={inbox} alt="inbox" />
-          <div className="left-align-section">
-            {" "}
-            {/* Apply the class to the section */}
-            <h3 className="underlined">Inbox</h3>
-            <p className="paragraph">
-              Keep in touch with new connections and nurture existing ones with
-              your Communiti inbox.
-            </p>
+          <div className="how_inbox_section">
+            <img className="how_image" src={inbox} alt="inbox" />
+            <div className="left-align-section">
+              <h3 className="underlined">Inbox</h3>
+              <p className="paragraph">
+                Keep in touch with new connections and nurture existing ones
+                with your Communiti inbox.
+              </p>
+            </div>
           </div>
-          <img className="how_image" src={update} alt="update" />
-          <div className="left-align-section">
-            {" "}
-            {/* Apply the class to the section */}
-            <h3 className="underlined">Updates</h3>
-            <p className="paragraph">
-              Subscribe for newsletters and announcements from the organizations
-              you're a part of so you can keep up with the latest info.
-            </p>
+          <div className="how_updates_section">
+            <img className="how_image" src={update} alt="update" />
+            <div className="left-align-section">
+          
+              <h3 className="underlined">Updates</h3>
+              <p className="paragraph">
+                Subscribe for newsletters and announcements from the
+                organizations you're a part of so you can keep up with the
+                latest info.
+              </p>
+            </div>
           </div>
         </div>
       </div>
