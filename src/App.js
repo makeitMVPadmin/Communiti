8572875c9cbfbc31120eefc6a70be12a9efa56c1
Anthony <anthony.s.quispe@gmail.com>
2023-11-04 18:49:58 -0400

    Merge Develop branch on to existing branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import "./styles/_global.scss";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<LandingPage />} />
     </Routes>
   );
 }
