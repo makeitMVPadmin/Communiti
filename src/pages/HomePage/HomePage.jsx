ab8e051dabcdb6568bb01668680b94e51cfcea40
dre <andreadrematos@gmail.com>
2023-11-05 12:02:03 -0500

    fixed up mobile, preparing for tablet

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,37 +9,42 @@ import apple from '../../assets/logos/apple.svg';
 
 function HomePage() {
     return (
-        <main className='home'>
-            <img src={communitiLong} alt="logo for Communiti" className='home__logo'></img>
+        <>
+            <header className='header-main'>
+                <img src={communitiLong} alt="logo for Communiti" className='header-main__logo'></img>
+            </header>
 
-            <div className='home__image'>
-            </div>
-
-            <h1 className='home__heading'>
-                Welcome to your 
-                <div className='home__heading-comminuti'>
-                    {/* <p>Communiti!</p> */}
-                    <span className='yellow-shade'>Communiti!</span>
+            <main className='home'>
+                <div className='home__image'>
                 </div>
-            </h1>
 
-            <button className='home__button home__button--login'>Log In</button>
-            <button className='home__button home__button--signup'>Sign Up</button>
+                <section className='home__content'>
+                    <h1 className='home__heading'>
+                        Welcome to your Communiti!
+                    </h1>
+                    <div className='home__buttons'>
+                        <button className='home__buttons-button home__buttons-button--login'>Log In</button>
+                        <button className='home__buttons-button home__buttons-button--signup'>Sign Up</button>
+                    </div>
 
-            <p className='home__writing'>or</p>
+                    <p className='home__writing'>or</p>
 
-            <div className='home__alt-buttons'>
-                <div className='home__alt-buttons-button'>
-                    <img src={google} alt='logo for Google' className='home__icon'></img>
-                </div>
-                <div className='home__alt-buttons-button'>
-                    <img src={facebook} alt='logo for Facebook' className='home__icon'></img>
-                </div>
-                <div className='home__alt-buttons-button'>
-                <img src={apple} alt='logo for Apple' className='home__icon'></img>
-                </div>
-            </div>
-        </main>
+                    <div className='home__alt-buttons'>
+                        <div className='home__alt-buttons-button'>
+                            <img src={google} alt='logo for Google' className='home__icon'></img>
+                        </div>
+                        <div className='home__alt-buttons-button'>
+                            <img src={facebook} alt='logo for Facebook' className='home__icon'></img>
+                        </div>
+                        <div className='home__alt-buttons-button'>
+                        <img src={apple} alt='logo for Apple' className='home__icon'></img>
+                        </div>
+                    </div>
+                </section>
+                
+            </main>
+        </>
+        
     )
 }
 
