bb58713deee46e5ace7419e38ebeb11a2016ba12
dre <andreadrematos@gmail.com>
2023-11-05 10:14:55 -0500

    set up route and got everything on page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,41 @@
+import './HomePage.scss';
+
+import communitiLong from '../../assets/logos/communiti2.svg';
+import google from '../../assets/logos/google.svg';
+import facebook from '../../assets/logos/facebook.svg';
+import apple from '../../assets/logos/apple.svg';
+
+// import { Link } from 'react-router-dom';
+
+function HomePage() {
+    return (
+        <main className='home'>
+            <img src={communitiLong} ></img>
+            <div className='home__image'>
+            </div>
+
+            <h1 className='home__heading'>
+                Welcome to your Communiti!
+            </h1>
+
+            <button className='home__button home__button--login'>Log In</button>
+            <button className='home__button home__button--signup'>Sign Up</button>
+
+            <p>or</p>
+
+            <div className='home__alt-buttons'>
+                <div className='home__alt-buttons-button'>
+                    <img src={google} alt='logo for Google' className='home__icon'></img>
+                </div>
+                <div className='home__alt-buttons-button'>
+                    <img src={facebook} alt='logo for Facebook' className='home__icon'></img>
+                </div>
+                <div className='home__alt-buttons-button'>
+                <img src={apple} alt='logo for Apple' className='home__icon'></img>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default HomePage;
