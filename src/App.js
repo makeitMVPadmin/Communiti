19d73019ab4781a9af3a89a79bf2e0a85d7fc42f
dre <andreadrematos@gmail.com>
2023-11-05 22:37:26 -0500

    finished styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/_global.scss";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import HomePage from "./pages/HomePage/HomePage";
+import LogIn from "./pages/LogIn/LogIn";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path='/home' element={<HomePage />} />
+      <Route path="/login" element={<LogIn />} />
     </Routes>
   );
 }
