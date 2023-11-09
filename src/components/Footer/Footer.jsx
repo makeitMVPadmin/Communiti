8b3f4bc4bea747a3c7453967b5f608d3208436e5
Aurelie Gedeon <agedeon540@gmail.com>
2023-11-08 22:37:26 -0600

    87-AG Added button next to logo; will fix flexbox issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import "./Footer.scss";
 import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 import instagram from "../../assets/logos/instagram.svg";
 import twitter from "../../assets/logos/twitter.svg";
 import linkedin from "../../assets/logos/linkedin.svg";
@@ -8,7 +9,10 @@ import communiti2 from "../../assets/logos/communiti2.svg";
 function Footer() {
   return (
     <div className="footer">
-      <img className="logo" src={communiti2} alt="communiti in black font" />
+      <div className="logo-container">
+        <img className="logo" src={communiti2} alt="communiti in black font" />
+        <Button buttonText="Join the Launch" className="button button--yellow" />
+      </div>
       <div className="separator"></div>
       <div className="icon-row">
         <Link>
