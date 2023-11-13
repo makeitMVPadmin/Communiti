f9d03993bdf1bcdae3f9cf7a35df64b05402d207
Anthony <anthony.s.quispe@gmail.com>
2023-11-13 10:53:40 -0500

    Pushing DashboardNavbar

--- src/Firebase/FirebaseAuth.jsx
+++ src/Firebase/FirebaseAuth.jsx
@@ -6,37 +6,65 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { auth } from "./FirebaseConfig"; // Import the Firebase authentication object from FirebaseConfig
+import { updateUserInFirestore } from "./FirebaseStore";
 
 // Export a function to handle user sign-up
-export const handleSignUp = (email, password) => {
-  // Use createUserWithEmailAndPassword from Firebase auth to create a new user account
-  return createUserWithEmailAndPassword(auth, email, password).catch(
-    (error) => {
-      const errorCode = error.code; // Store the error code
-      const errorMessage = error.message; // Store the error message
-      // Handle the error or display an error message to the user.
-    }
-  );
+export const handleSignUp = async (fullName, email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const { user } = userCredential;
+
+    // Update or create user in Firestore
+    await updateUserInFirestore(user);
+  } catch (error) {
+    // Handle the error or display an error message to the user.
+    console.error("Sign-up error:", error);
+  }
 };
 
 // Export a function to handle user sign-in
-export const handleSignIn = (email, password) => {
-  // Use signInWithEmailAndPassword from Firebase auth to sign in a user
-  return signInWithEmailAndPassword(auth, email, password).catch((error) => {
-    const errorCode = error.code; // Store the error code
-    const errorMessage = error.message; // Store the error message
+export const handleSignIn = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const { user } = userCredential;
+
+    // Update user in Firestore on sign-in (if needed)
+    await updateUserInFirestore(user);
+  } catch (error) {
     // Handle the error or display an error message to the user.
-  });
+    console.error("Sign-in error:", error);
+  }
 };
 
 // Export a function to handle Google sign-in
-export const handleGoogleSignIn = () => {
-  const provider = new GoogleAuthProvider(); // Create a GoogleAuthProvider instance
-  provider.addScope("https://www.googleapis.com/auth/contacts.readonly"); // Add a scope
-  // Use signInWithPopup from Firebase auth to sign in with Google
-  return signInWithPopup(auth, provider).catch((error) => {
-    const errorCode = error.code; // Store the error code
-    const errorMessage = error.message; // Store the error message
+export const handleGoogleSignIn = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+
+    // Sign in with Google and get user credentials
+    const result = await signInWithPopup(auth, provider);
+    const { user } = result;
+
+    // Extract additional information
+    const { email, photoURL, displayName } = user;
+
+    // Update or create user in Firestore with additional information
+    await updateUserInFirestore(user, {
+      email,
+      photoURL,
+      fullName: displayName,
+    });
+  } catch (error) {
     // Handle the error or display an error message to the user.
-  });
+    console.error("Google sign-in error:", error);
+  }
 };
