fa2fd381f0e7f5b6a69b3d5269fc2bf950eea93e
Anthony <anthony.s.quispe@gmail.com>
2023-11-13 01:00:10 -0500

    Updated Styling for the create communiti

--- src/components/CommunitiesComponents/CreateCommuniti2/CreateCommuniti2.jsx
+++ src/components/CommunitiesComponents/CreateCommuniti2/CreateCommuniti2.jsx
@@ -1,9 +1,14 @@
-// CreateCommuniti2.jsx
-import React, { useState } from "react";
+import { useState } from "react";
 import "./CreateCommuniti2.scss";
 import calendarImage from "../../../assets/images/calendar.svg";
+import Button from "../../../components/Button/Button";
 
-function CreateCommuniti2({ communitiDescription, setCommunitiDescription }) {
+function CreateCommuniti2({
+  communitiDescription,
+  setCommunitiDescription,
+  handleBack,
+  handleNext,
+}) {
   const maxLength = 500;
   const [text, setText] = useState(communitiDescription);
 
@@ -16,43 +21,59 @@ function CreateCommuniti2({ communitiDescription, setCommunitiDescription }) {
   };
 
   return (
-    <div className="create-communiti2__content">
-      <div className="create-communiti2__content-container">
-        <div className="create-communiti2__content-container-left">
-          <h1 className="create-communiti2__content-heading">
-            Welcome to your Communiti!
-          </h1>
+    <div className="create-communiti2">
+      <div className="create-communiti2__content">
+        <div className="create-communiti2__content-container">
+          <div className="create-communiti2__content-container-left">
+            <h1 className="create-communiti2__content-heading">
+              Welcome to your Communiti!
+            </h1>
+          </div>
+          <div className="create-communiti2__content-container-right">
+            <img
+              className="create-communiti2__content-image"
+              src={calendarImage}
+              alt="Calendar"
+            />
+          </div>
         </div>
-        <div className="create-communiti2__content-container-right">
-          <img
-            className="create-communiti2__content-image"
-            src={calendarImage}
-            alt="Calendar"
-          />
-        </div>
+        <form className="create-communiti2__content-form">
+          <label
+            className="create-communiti2__content-label"
+            htmlFor="communitiDescription"
+          >
+            How would you describe your Communiti?
+          </label>
+          <div className="create-communiti2__content-textarea-container">
+            <textarea
+              className="create-communiti2__content-textarea"
+              placeholder="Communiti Description"
+              id="communitiDescription"
+              name="communitiDescription"
+              value={text}
+              onChange={handleInputChange}
+              maxLength={maxLength}
+            />
+            <p className="create-communiti2__content-character-count">
+              {maxLength - text.length}/500 characters
+            </p>
+          </div>
+        </form>
       </div>
-      <form className="create-communiti2__content-form">
-        <label
-          className="create-communiti2__content-label"
-          htmlFor="communitiDescription"
-        >
-          How would you describe your Communiti?
-        </label>
-        <div className="create-communiti2__content-textarea-container">
-          <textarea
-            className="create-communiti2__content-textarea"
-            placeholder="Communiti Description"
-            id="communitiDescription"
-            name="communitiDescription"
-            value={text}
-            onChange={handleInputChange}
-            maxLength={maxLength}
-          />
-          <p className="create-communiti2__content-character-count">
-            {maxLength - text.length}/500 characters
-          </p>
-        </div>
-      </form>
+      <div className="create-communiti1__container-bottom">
+        <Button
+          buttonText="Back"
+          className={`button button-back`}
+          onClick={handleBack}
+        />
+        <Button
+          buttonText="Next"
+          className={`button create-communiti__button-next ${
+            !communitiDescription ? "button__not-active" : "button__active"
+          }`}
+          onClick={handleNext}
+        />
+      </div>
     </div>
   );
 }
