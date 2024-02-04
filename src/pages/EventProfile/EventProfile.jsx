690136b55e76a58b081aaf884feed385dd5c8a2a
Jonathan <jpreuven@gmail.com>
2024-02-04 06:19:22 -0800

    finished styling events page

--- src/pages/EventProfile/EventProfile.jsx
+++ src/pages/EventProfile/EventProfile.jsx
@@ -14,146 +14,117 @@ export default function EventProfile() {
     <>
       {/* <DashboardNavbar /> */}
       <main className="event-profile">
-        <div className="event-profile-container">
-          {/* <div
-            style={{ display: "flex", flexDirection: "column", width: "100%" }}
-          > */}
-          {/* <div
-          className="event-profile-image-container"
-            style={{
-              width: "100%",
-            }}
-          > */}
-          <img className="event-profile-image" src={placeHolderIcon} />
-          {/* </div> */}
-          <div className="event-profile-header-container">
-            <h1 className="event-profile-header">
+        <div className="event-profile__container">
+          <img className="event-profile__image" src={placeHolderIcon} />
+          <div className="event-profile__header-container">
+            <h1 className="event-profile__header">
               How AI can help in Marketing
             </h1>
-            <span className="event-profile-header-trash-icon">🗑️</span>
+            <span className="event-profile__header-trash-icon">&#128465;</span>
           </div>
-          <div className="event-profile-content-edit-container">
-            <div className="event-profile-content-container">
-              <p className="event-profile-content-text">
+          <div className="event-profile__content-edit-container">
+            <div className="event-profile__content-container">
+              <p className="event-profile__content-text">
                 Come learn about different ways you can meet investors and learn
                 how to pitch to them Come learn about different ways you can
                 meet investors and learn how to pitch to them
               </p>
-              <div className="event-profile-content-details-container">
-                <div className="event-profile-content-details-div">
+              <div className="event-profile__content-details-container">
+                <div className="event-profile__content-details-div">
                   <img
                     src={calendarIcon}
-                    className="event-profile-content-details-image"
+                    className="event-profile__content-details-image"
                   />
-                  <p className="event-profile-content-details-text">
+                  <p className="event-profile__content-details-text">
                     WED, NOV 7
                   </p>
                 </div>
-                <div className="event-profile-content-details-div">
+                <div className="event-profile__content-details-div">
                   <img
                     src={clockIcon}
-                    className="event-profile-content-details-image"
+                    className="event-profile__content-details-image"
                   />
-                  <p className="event-profile-content-details-text">
+                  <p className="event-profile__content-details-text">
                     4:00 PM - 5:00 PM PST
                   </p>
                 </div>
-                <div className="event-profile-content-details-div">
+                <div className="event-profile__content-details-div">
                   <img
                     src={location}
-                    className="event-profile-content-details-image"
+                    className="event-profile__content-details-image"
                   />
-                  <p className="event-profile-content-details-text">
+                  <p className="event-profile__content-details-text">
                     213 Seymour st., Denver
                   </p>
                 </div>
               </div>
             </div>
-            <div className="event-profile-edit-container">
-              <button
-                style={{
-                  margin: "0",
-                  background: "rgb(9, 84, 176)",
-                  color: "white",
-                  width: "170px",
-                  height: "44px",
-                  border: "none",
-                  borderRadius: "10px",
-                  fontFamily: "Corben",
-                }}
-              >
-                Edit Event
-              </button>
+            <div className="event-profile__edit-container">
+              <button className="event-profile__edit-button">Edit Event</button>
             </div>
           </div>
-          <div className="event-profile-communiti">
-            <h3>From the community</h3>
-            <div className="communiti__card">
-              <img
-                // src={community?.CommunityImage || placeHolderIcon}
-                src={placeHolderIcon}
-                className="communiti__card-profile-pic"
-              />
-              <div className="communiti__card-bottom-container">
-                <div className="communiti__card-bottom-inner-container">
-                  <div>
-                    <h6 className="communiti__card-heading">Community Name</h6>
-                  </div>
-                  <div className="communiti__card-button-div-container">
-                    <button
-                      className="communiti__card-arrow-button"
-                      //   onClick={() =>
-                      //     navigate(`/communities/admin/${community?.id}`)
-                      //   }
-                    >
-                      <img
-                        src={rightArrowIcon}
-                        alt="right arrow button"
-                        className="communiti__card-arrow-button-img"
-                      />
-                    </button>
+          <div className="event-profile__communiti-organizer-container">
+            <div className="event-profile__communiti">
+              <h3 className="event-profile__communiti-header">
+                From the community
+              </h3>
+              <div className="event-profile__communiti-card">
+                <img
+                  // src={community?.CommunityImage || placeHolderIcon}
+                  src={placeHolderIcon}
+                  className="event-profile__communiti-card-profile-pic"
+                />
+                <div className="event-profile__communiti-card-bottom-container">
+                  <div className="event-profile__communiti-card-bottom-inner-container">
+                    <div>
+                      <h6 className="event-profile__communiti-card-heading">
+                        Community Name
+                      </h6>
+                    </div>
+                    <div className="event-profile__communiti-card-button-div-container">
+                      <button
+                        className="event-profile__communiti-card-arrow-button"
+                        //   onClick={() =>
+                        //     navigate(`/communities/admin/${community?.id}`)
+                        //   }
+                      >
+                        <img
+                          src={rightArrowIcon}
+                          alt="right arrow button"
+                          className="event-profile__communiti-card-arrow-button-img"
+                        />
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div>
-            <h3>Organized By</h3>
-            <div
-              style={{
-                display: "flex",
-                border: "1px solid black",
-                maxWidth: "300px",
-                padding: "4px 8px",
-                borderRadius: "10px",
-              }}
-            >
-              <div style={{ display: "flex" }}>
-                <img src={profilePic} style={{ width: "40px" }} />
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marginLeft: "10px",
-                  flexGrow: "1",
-                }}
-              >
-                <p style={{ fontSize: "14px" }}>Marina Reese</p>
-                <p style={{ fontSize: "14px" }}>Product Manager</p>
-              </div>
-              <div style={{ display: "flex" }}>
-                <img
-                  src={chatIcon}
-                  style={{
-                    width: "25px",
-                    // marginRight: "10px"
-                  }}
-                />
+            <div className="event-profile__organizer">
+              <h3 className="event-profile__communiti-header">Organized By</h3>
+              <div className="event-profile__organizer-card">
+                <div className="event-profile__organizer-card-profile-pic-container">
+                  <img
+                    className="event-profile__organizer-card-profile-pic"
+                    src={profilePic}
+                  />
+                </div>
+                <div className="event-profile__organizer-card-details">
+                  <p className="event-profile__organizer-card-details-paragraph-name">
+                    Marina Reese
+                  </p>
+                  <p className="event-profile__organizer-card-details-paragraph-title">
+                    Product Manager
+                  </p>
+                </div>
+                <div className="event-profile__organizer-card-details-chat-emoji-container">
+                  <img
+                    className="event-profile__organizer-card-details-chat-emoji"
+                    src={chatIcon}
+                  />
+                </div>
               </div>
             </div>
           </div>
-          {/* </div> */}
         </div>
       </main>
     </>
