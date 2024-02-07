a067fd4bce62ee2262ed673f51189f6dfc2815d1
Shannon Lin <shannon.n.lin@gmail.com>
2024-02-06 16:17:21 -0800

    Add useEffect to ensure auth is initialized before fetching user data

--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,43 +1,57 @@
-import React, {children, useState, createContext, useContext, useEffect} from 'react';
+import { children, useState, createContext, useContext, useEffect } from 'react';
 import { collection, doc, getDoc } from "firebase/firestore";
 import { db, auth } from "../Firebase/FirebaseConfig";
+
 export const AppContext = createContext(null);
 
-
 export default function AppContextProvider({children}) {
+  const [userData, setUserData] = useState(null);
+  const [authInitialized, setAuthInitialized] = useState(false);
 
-    const [user, setUser] = useState(null);
+  // Ensure authentication is initialized before fetching user data
+  useEffect(() => { 
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setAuthInitialized(true);
+      } else {
+        setAuthInitialized(true);
+      }
+    });
 
-    useEffect(() => {
-        const currentUser = auth.currentUser;
-      const fetchUserData = async () => {
-        try {
-          console.log("test, appContextpage");
-          const uid = currentUser.uid;
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        console.log("test, appContextpage");
+        if (authInitialized) {
+          const currentUser = auth.currentUser;
 
           if (currentUser) {
+            const uid = currentUser.uid;
             const userDocRef = doc(collection(db, "Users"), uid);
             const userDocSnapshot = await getDoc(userDocRef);
 
             if (userDocSnapshot.exists()) {
-              const fullName = userDocSnapshot.data().fullName;
-              setUser(fullName);
-            } else {
-              console.error("User document does not exist");
+              setUserData(userDocSnapshot.data());
             }
+
+          } else {
+            console.error("User document does not exist");
           }
-        } catch (error) {
-          console.error("Error fetching user data:", error);
         }
-      };
-      fetchUserData();
-    }, []);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, [authInitialized]);
 
   return (
     <AppContext.Provider
       value={{
-        isLoading: false,
-        user: "Bob",
+        userData: userData,
       }}
     >
       {children}
