79bb6fcced8bf88971bca65b6a0771416ebd8f1f
Ana Bennett <anabennett1111@gmail.com>
2024-02-21 19:17:39 -0600

    Changed the welcome back text

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,64 +1,31 @@
 import "./DashboardPage.scss";
 import DashboardNavbar from "../../components/DashboardNavbar/DashboardNavbar";
 import plusIcon from "../../assets/images/plusIcon.svg";
-import { collection, doc, getDoc } from "firebase/firestore";
-import { db, auth } from "../../Firebase/FirebaseConfig";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Announcements from "../../components/AnnouncementsDashboard/Announcements";
+import data from "../../data.json";
 
 function DashboardPage() {
   const navigate = useNavigate();
   const [userCommunities, setUserCommunities] = useState([]);
+  // const [userFullName, setUserFullName] = useState(
+  //   data.users[1].fullName || null
+  // );
 
-  const [userFullName, setUserFullName] = useState(() => {
-    const storedFullName = sessionStorage.getItem("FullName");
-    return storedFullName ? JSON.parse(storedFullName) : null;
-  });
 
-  useEffect(() => {
-    const currentUser = auth.currentUser;
 
-    const fetchUserData = async () => {
-      try {
-        const uid = currentUser.uid;
-
-        if (currentUser) {
-          const userDocRef = doc(collection(db, "Users"), uid);
-          const userDocSnapshot = await getDoc(userDocRef);
-
-          if (userDocSnapshot.exists()) {
-            const fullName = userDocSnapshot.data().fullName;
-            setUserFullName(fullName);
-          } else {
-            console.error("User document does not exist");
-          }
-        }
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
-  }, []);
+  //   const storedFullName = sessionStorage.getItem("FullName");
+  //   console.log("Stored FullName:", storedFullName);
+  //   return storedFullName ? JSON.parse(storedFullName) : null;
+  // });
 
   useEffect(() => {
-    // Fetch user's communities
-    const fetchUserCommunities = async () => {
+ 
+    const fetchUserCommunities = () => {
       try {
-        const currentUser = auth.currentUser;
-        if (currentUser) {
-          const uid = currentUser.uid;
-          const userDocRef = doc(collection(db, "Users"), uid);
-          const userDocSnapshot = await getDoc(userDocRef);
-
-          if (userDocSnapshot.exists()) {
-            const userCommunitiesIds =
-              userDocSnapshot.data().CommunitiesJoined || [];
-
-            setUserCommunities(userCommunitiesIds);
-          }
-        }
+        const userCommunitiesIds = data.CommunitiesJoined || [];
+        setUserCommunities(userCommunitiesIds);
       } catch (error) {
         console.error("Error fetching user communities:", error);
       }
@@ -68,7 +35,6 @@ function DashboardPage() {
   }, []);
 
   const handleCreateCommunity = () => {
-    // Load the specified URL on button click
     navigate("/communities/create");
   };
 
@@ -78,7 +44,7 @@ function DashboardPage() {
       <div className="dashboard-page__container">
         <div className="dashboard-page__welcome">
           <h1 className="dashboard-page__header">
-            Welcome back! {userFullName ? `${userFullName} 👋` : "Loading..."}
+            Welcome back 👋`
           </h1>
         </div>
         {userCommunities.length === 0 && (
